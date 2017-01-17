0672e55008f2a1d0d6e5b6634e9020c0e4bc21a1
Dennis Mwangi <githinji.mwangi@gmail.com>
2017-01-17 15:18:15 +0300

    second commit

--- primenumbers.py
+++ primenumbers.py
@@ -1,3 +1,11 @@
 #A working function to generate prime numbers from 0 to n with asymptotic analysis in your public Github repo
 
 #A Prime Number can be divided evenly only by 1, or itself. And it must be a whole number greater than 1.
+
+def prime_numbers(num):
+	if num == 1:
+		return False
+	
+
+
+
