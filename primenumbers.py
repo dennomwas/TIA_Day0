b8e6947f802a3a04173342cef27541b065fcb154
Dennis Mwangi <githinji.mwangi@gmail.com>
2017-01-17 16:07:28 +0300

    third commit

--- primenumbers.py
+++ primenumbers.py
@@ -2,9 +2,28 @@
 
 #A Prime Number can be divided evenly only by 1, or itself. And it must be a whole number greater than 1.
 
-def prime_numbers(num):
-	if num == 1:
-		return False
+def prime_numbers(n):
+    for i in range(2, n):
+        if n%i == 0:
+            return False
+    return True
+
+
+n = 50
+
+for p in range(2, n+1):
+    if prime_numbers(p):
+        print (p),
+
+print ("Done")
+	
+
+
+
+
+
+	
+
 	
 
 
